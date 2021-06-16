1083ff6e883e05819f6ecd18ed80d51d66f58f35
Morten Pedersen <mortenvester1@gmail.com>
2021-06-15 20:19:04 -0400

    update setup

--- setup.py
+++ setup.py
@@ -1,38 +1,33 @@
-from setuptools import setup
+from setuptools import setup, Extension
 from Cython.Build import cythonize
-from numpy.distutils.misc_util import Configuration
-from distutils.core import Extension
-import numpy as np
-import os
 import glob
 
-# os.environ["CXX"] = "g++-9"
-# os.environ["CC"] = "gcc"
-
-
-# def configuration(parent_package='', top_path=None):
-#     config = Configuration(None, parent_package, top_path)
-#     config.add_subpackage('cubist_src')
-#     return config
-
-
-exts = [Extension(name='_cubist',
-                  sources=["cubist/_cubist.pyx"] + glob.glob("cubist/src/*.c"),
-                  include_dirs=["cubist/src"])]
-
-
-if __name__ == "__main__":
-    setup(
-        name="cubist",
-        ext_modules=cythonize(exts),
-        zip_safe=False,
-        # configuration=configuration,
-        include_package_data=True,
-        # install_requires = [
-        #     "numpy>=1.19.2"
-        #     "pandas>=1.1.3",
-        #     "setuptools>=52.0.0",
-        #     "cython>=0.29.23",
-        #     "scikit-learn>=0.24.2"
-        # ]
+exts = [
+    Extension(
+        name='_cubist',
+        sources=["cubist/_cubist.pyx"] + glob.glob("cubist/src/*.c"),
+        include_dirs=["cubist/src"]
     )
+]
+
+with open("README.md", 'r') as f:
+    long_description = f.read()
+
+setup(
+    name="cubist",
+    version="0.0.1",
+    description="A python port of the R library cubist.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    ext_modules=cythonize(exts),
+    zip_safe=False,
+    # configuration=configuration,
+    include_package_data=True,
+    install_requires=[
+        "cython>=0.29.23",
+        # "numpy>=1.19.2"
+        # "pandas>=1.1.3",
+        # "setuptools>=52.0.0",
+        # "scikit-learn>=0.24.2"
+    ]
+)
