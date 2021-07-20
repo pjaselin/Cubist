337bd3a66f6b62559db103b1df357ab9ef7fc4d3
Patrick Aselin <26674234+pjaselin@users.noreply.github.com>
2021-07-20 16:53:47 -0400

    Revert "Completing translation"

--- cubist/_quinlan_attributes.py
+++ cubist/_quinlan_attributes.py
@@ -3,8 +3,10 @@ from pandas.api.types import is_string_dtype, is_numeric_dtype, is_datetime64_an
 
 
 def _get_data_format(x: pd.Series):
-    """
-    Function to obtain the data type/formatting information for a Pandas Series
+    """Function to obtain the data formatting information for a Pandas Series
+
+    :param x: Input Pandas Series
+    :return: Formatting information about the Series
     """
     # remove NAs from series
     x = x.dropna()
@@ -23,7 +25,9 @@ def _get_data_format(x: pd.Series):
 
 
 def quinlan_attributes(df: pd.DataFrame) -> dict:
-    """
-    Function to collect the data formatting information for each column in a Pandas DataFrame
+    """Function to collect the data formatting information for each column in a Pandas DataFrame
+
+    :param df: Input Pandas DataFrame
+    :return: Dictionary with column names as keys and data format information as values
     """
     return {col_name: _get_data_format(col_data) for col_name, col_data in df.iteritems()}
