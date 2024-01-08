a82728810f73a3659ce279df18c1a5f7c005bb94
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-01-07 19:28:16 -0500

    removing 0 from the if pd.isna line

--- cubist/_utils.py
+++ cubist/_utils.py
@@ -5,8 +5,8 @@ import numpy as np
 def _format(x: float, digits: int = 15) -> str:
     """Python version of the R format function to return a number formatted as a
     string rounded to `digits` number of digits from the left."""
-    # if x is NA or 0 return NA
-    if pd.isna(x) or x == 0:
+    # if x is NA return NA
+    if pd.isna(x):
         return x
     if np.iscomplex(x):
         raise ValueError("Complex numbers not supported")
