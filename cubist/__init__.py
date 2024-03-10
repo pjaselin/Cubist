922fe495c3051dd1e8560d8414bd69ad1fb94e4b
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-03-10 16:23:46 -0400

    placing version in pyproject.toml and dropping attr

--- cubist/__init__.py
+++ cubist/__init__.py
@@ -2,20 +2,18 @@
 cubist: Cubist regression model for Python
 ==========================================
 
-cubist is a Python module that wraps the C implementation of Quinlan's 
-regression model of the same name. In the module, "Cubist" is the name of the 
-model class. 
+cubist is a Python module that wraps the C implementation of Quinlan's
+regression model of the same name. In the module, "Cubist" is the name of the
+model class.
 
-The model requires input data to be particularly formattted as strings and this 
-module handles this formatting process as well as the model options. 
+The model requires input data to be particularly formattted as strings and this
+module handles this formatting process as well as the model options.
 
-The module is designed after the sklearn API and inherits from the 
+The module is designed after the sklearn API and inherits from the
 RegressorMixin and BaseEstimator classes, meaning that Cubist can be dropped
 into existing sklearn-based ML pipelines or scripts for experimenting.
 """
 from .cubist import Cubist
 from .exceptions import CubistError
 
-from .__version__ import __version__
-
-__all__ = ['Cubist', 'CubistError', '__version__']
+__all__ = ["Cubist", "CubistError"]
--- setup.py
+++ setup.py
@@ -1,25 +1,8 @@
 import glob
-import os.path
-import codecs
 
 from Cython.Build import cythonize
 from setuptools import setup, Extension
 import numpy as np
-
-
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), "r") as fp:
-        return fp.read()
-
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    raise RuntimeError("Unable to find version string.")
-
 
 extensions = [
     Extension(
@@ -34,5 +17,4 @@ setup(
     ext_modules=cythonize(
         extensions, compiler_directives={"language_level": 3, "profile": False}
     ),
-    # version=get_version("cubist/__version__.py"),
 )
