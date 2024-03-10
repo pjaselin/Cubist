64a2331bfde1c30fe86f06d26c7ff63f7b4ee063
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-03-09 20:20:58 -0500

    reading version in setup.py again

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 import glob
+import os.path
+import codecs
 
 from Cython.Build import cythonize
 from setuptools import setup, Extension
 import numpy as np
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
 
 extensions = [
     Extension(
@@ -17,4 +34,5 @@ setup(
     ext_modules=cythonize(
         extensions, compiler_directives={"language_level": 3, "profile": False}
     ),
+    version=get_version("cubist/__version__.py"),
 )
