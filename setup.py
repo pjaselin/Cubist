c21e87e49cae8c47bc0e35eac69b5d5b2b7de7c5
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-03-09 19:49:00 -0500

    naming cubist in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
 import glob
-# import os.path
-# import codecs
 
 from Cython.Build import cythonize
 from setuptools import setup, Extension
@@ -15,54 +13,7 @@ extensions = [
     )
 ]
 
-# with open("README.md", "r", encoding="utf-8") as f:
-#     long_description = f.read()
-
-# with open("requirements.txt", "r", encoding="utf-8") as f:
-#     requires = f.read()
-
-
-# def read(rel_path):
-#     here = os.path.abspath(os.path.dirname(__file__))
-#     with codecs.open(os.path.join(here, rel_path), "r") as fp:
-#         return fp.read()
-
-
-# def get_version(rel_path):
-#     for line in read(rel_path).splitlines():
-#         if line.startswith("__version__"):
-#             delim = '"' if '"' in line else "'"
-#             return line.split(delim)[1]
-#     raise RuntimeError("Unable to find version string.")
-
-
 setup(
-    # version=get_version("cubist/__version__.py"),
-    # author="Patrick Aselin",
-    # description="A Python package for fitting Quinlan's Cubist regression model.",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
+    name="cubist",
     ext_modules=cythonize(extensions),
-    # zip_safe=False,
-    # include_package_data=True,
-    # install_requires=requires,
-    # extras_require={
-    #     "dev": ["pre-commit", "pytest", "ruff", "coverage"],
-    # },
-    # url="https://github.com/pjaselin/Cubist",
-    # packages=["cubist"],
-    # license="GPL v3",
-    # classifiers=[
-    #     "Programming Language :: Python",
-    #     "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-    #     "Topic :: Software Development :: Libraries",
-    #     "Topic :: Scientific/Engineering :: Artificial Intelligence",
-    #     "Topic :: Scientific/Engineering :: Mathematics",
-    #     "Intended Audience :: Developers",
-    #     "Intended Audience :: Science/Research",
-    #     "Programming Language :: Python :: 3.8",
-    #     "Programming Language :: Python :: 3.9",
-    #     "Programming Language :: Python :: 3.10",
-    #     "Programming Language :: Python :: 3.11",
-    # ],
 )
