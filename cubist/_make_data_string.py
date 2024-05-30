29bb37804dadfb80a88c2af46d101220f0cfd121
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-05-29 21:03:09 -0400

    adding comments on pandas feature support

--- cubist/_make_data_string.py
+++ cubist/_make_data_string.py
@@ -64,7 +64,8 @@ def _make_data_string(x, y=None, w=None):
             x[col] = x[col].astype(str)
 
     # remove leading whitespace from all elements
-    if hasattr(pd, "map"):
+    # handling pandas 2.2.2 feature change (applymap -> map)
+    if hasattr(x, "map"):
         x = x.map(lambda a: a.lstrip())
     else:
         x = x.applymap(lambda a: a.lstrip())
