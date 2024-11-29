bb15a1a011a787b9514b13b435b423754c6f6da2
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-11-29 17:39:17 -0500

    test_quinlan_attributes.py passes pylint

--- cubist/tests/test_quinlan_attributes.py
+++ cubist/tests/test_quinlan_attributes.py
@@ -1,3 +1,5 @@
+"""test obtaining quinlan attributes from input datasets"""
+
 import random
 
 import pandas as pd
@@ -13,6 +15,7 @@ from .._quinlan_attributes import (
 from .conftest import no_raise
 
 
+# create sample series for different data types
 int_series = pd.Series(random.sample(range(10, 30), 5))
 float_series = pd.Series(np.random.uniform(low=0.5, high=13.3, size=(5,)))
 str_series = pd.Series(["test0", "test1", "test2", "test3", "test4"])
@@ -38,6 +41,7 @@ bad_df = pd.DataFrame(
     ],
 )
 def test_is_all_int_dtype(test_input, expected):
+    """test integer data type"""
     assert _is_all_int_dtype(test_input) == expected
 
 
@@ -52,6 +56,7 @@ def test_is_all_int_dtype(test_input, expected):
     ],
 )
 def test_is_all_float_dtype(test_input, expected):
+    """test float data type"""
     assert _is_all_float_dtype(test_input) == expected
 
 
@@ -67,6 +72,7 @@ def test_is_all_float_dtype(test_input, expected):
     ],
 )
 def test_get_data_format(test_input, expected, raises):
+    """test classifying data types"""
     with raises:
         try:
             assert any(_get_data_format(test_input) == expected)
@@ -81,5 +87,6 @@ def test_get_data_format(test_input, expected, raises):
     [(good_df, dict, no_raise()), (bad_df, dict, pytest.raises(ValueError))],
 )
 def test_quinlan_attributes(test_input, expected, raises):
+    """test obtaining quinlan attributes"""
     with raises:
-        assert type(_quinlan_attributes(test_input)) is expected
+        assert isinstance(_quinlan_attributes(test_input), expected)
