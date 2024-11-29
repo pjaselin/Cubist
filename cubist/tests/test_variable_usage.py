ec6d7a1524bb021b48d2727e66594ee8f1d93c87
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-11-29 17:41:15 -0500

    test_variable_usage.py passes pylint

--- cubist/tests/test_variable_usage.py
+++ cubist/tests/test_variable_usage.py
@@ -1,8 +1,11 @@
+"""test variable usage functions"""
+
 import pytest
 
 from .._variable_usage import _variable_usage
 
 
 def test_variable_usage():
+    """test that variable usage function works"""
     with pytest.raises(ValueError):
         _variable_usage("", ["A"])
