e92f92cb16d9a1a18b0768185423248892b1cd15
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-11-29 17:23:27 -0500

    switch to local pylint

--- cubist/tests/test_cubist_display_mixin.py
+++ cubist/tests/test_cubist_display_mixin.py
@@ -1,3 +1,5 @@
+"""tests for _CubistDisplayMixin"""
+
 from sklearn.datasets import load_iris, fetch_california_housing
 import matplotlib.pyplot as plt
 import pytest
@@ -7,52 +9,53 @@ from ..cubist import Cubist
 
 
 def test_gridspec_kwargs_and_existing_plot():
-    fig, ax = plt.subplots()
-    X, y = load_iris(return_X_y=True, as_frame=True)
+    """test using gridspec_kwargs and passing an existing subplots object"""
+    _, ax = plt.subplots()
+    X, y = load_iris(return_X_y=True, as_frame=True)  # pylint: disable=C0103
     model = Cubist()
     model.fit(X, y)
-
     display = _CubistDisplayMixin()
-    display._validate_plot_params(
+    display._validate_plot_params(  # pylint: disable=W0212
         ax=ax, df=model.splits_, gridspec_kwargs={"hspace": 0.2}
     )
 
 
 def test_plot_grid_arrangement():
     """test adding one row to the number of rows needed when determining nrows/ncols"""
-    X, y = fetch_california_housing(return_X_y=True, as_frame=True)
+    X, y = fetch_california_housing(return_X_y=True, as_frame=True)  # pylint: disable=C0103
     model = Cubist()
-    # 5 columns will trigger adding one row to the number of rows needed
+    # only selecting 5 columns will trigger adding one row to the number of rows needed
     model.fit(X.iloc[:, :5], y)
-
     display = _CubistDisplayMixin()
-    display._validate_plot_params(df=model.splits_, gridspec_kwargs={"hspace": 0.2})
+    display._validate_plot_params(df=model.splits_, gridspec_kwargs={"hspace": 0.2})  # pylint: disable=W0212
 
 
 def test_validate_from_estimator_params_all_valid():
-    X, y = fetch_california_housing(return_X_y=True, as_frame=True)
+    """test using valie commmittee and rule parameters"""
+    X, y = fetch_california_housing(return_X_y=True, as_frame=True)  # pylint: disable=C0103
     model = Cubist(n_committees=5)
     model.fit(X, y)
-
     display = _CubistDisplayMixin()
-    display._validate_from_estimator_params(df=model.splits_, committee=2, rule=2)
+    display._validate_from_estimator_params(df=model.splits_, committee=2, rule=2)  # pylint: disable=W0212
 
 
 def test_validate_from_estimator_params_invalid_committee():
-    X, y = fetch_california_housing(return_X_y=True, as_frame=True)
+    """test using invalid committee parameter"""
+    X, y = fetch_california_housing(return_X_y=True, as_frame=True)  # pylint: disable=C0103
     model = Cubist(n_committees=5)
     model.fit(X, y)
-
     display = _CubistDisplayMixin()
+    # should raise TypeError
     with pytest.raises(TypeError):
-        display._validate_from_estimator_params(df=model.splits_, committee=2.0)
+        display._validate_from_estimator_params(df=model.splits_, committee=2.0)  # pylint: disable=W0212
 
 
 def test_validate_from_estimator_params_invalid_rule():
-    X, y = fetch_california_housing(return_X_y=True, as_frame=True)
+    """test using invalid rule parameter"""
+    X, y = fetch_california_housing(return_X_y=True, as_frame=True)  # pylint: disable=C0103
     model = Cubist(n_committees=5)
     model.fit(X, y)
-
     display = _CubistDisplayMixin()
+    # should raise TypeError
     with pytest.raises(TypeError):
         display._validate_from_estimator_params(df=model.splits_, rule=2.0)  # pylint: disable=W0212
