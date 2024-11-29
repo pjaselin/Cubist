2797a2defaffd5a31edcd7e898fb01841bfe46da
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-11-29 17:11:14 -0500

    test_common passes pylint

--- cubist/tests/test_common.py
+++ cubist/tests/test_common.py
@@ -1,5 +1,3 @@
-"""common scikit-learn estimator tests"""
-
 from sklearn.utils.estimator_checks import parametrize_with_checks
 
 from ..cubist import Cubist
@@ -7,4 +5,5 @@ from ..cubist import Cubist
 
 @parametrize_with_checks([Cubist()])
 def test_sklearn_compatible_estimator(estimator, check):
+    """perform common scikit-learn tests"""
     return check(estimator)
