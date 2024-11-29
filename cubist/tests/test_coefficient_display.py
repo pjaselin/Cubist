78a4352296a366fc6f0e4cf4e7a8287789d1a9dd
pjaselin <26674234+pjaselin@users.noreply.github.com>
2024-11-29 17:10:44 -0500

    test_coefficient_display passes pylint

--- cubist/tests/test_coefficient_display.py
+++ cubist/tests/test_coefficient_display.py
@@ -8,13 +8,12 @@ from cubist import Cubist, CubistCoefficientDisplay
 
 def test_coverage_display():
     """test creating the plot"""
-    X = pd.read_csv(
+    X = pd.read_csv(  # pylint: disable=C0103
         "https://raw.githubusercontent.com/selva86/datasets/refs/heads/master/BostonHousing.csv"
     )
     y = X.medv
-    X = X.drop(columns=["medv", "dis"])
-    # X, y = load_iris(return_X_y=True, as_frame=True)
-    model = Cubist()  # <- model parameters here, e.g. verbose=1
+    X = X.drop(columns=["medv", "dis"])  # pylint: disable=C0103
+    model = Cubist()
     model.fit(X, y)
 
     CubistCoefficientDisplay.from_estimator(model)
@@ -23,13 +22,11 @@ def test_coverage_display():
 
 def test_coverage_display_scatter_kwargs():
     """test scatter_kwargs parameter"""
-    X = pd.read_csv(
+    X = pd.read_csv(  # pylint: disable=C0103
         "https://raw.githubusercontent.com/selva86/datasets/refs/heads/master/BostonHousing.csv"
     )
     y = X.medv
     X = X.drop(columns=["medv", "dis"])
-
     model = Cubist()
     model.fit(X, y)
-
     CubistCoefficientDisplay.from_estimator(model, scatter_kwargs={"alpha": 0.5})
