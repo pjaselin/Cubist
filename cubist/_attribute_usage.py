027724778a751ab7558b0d4db4c617f5bd47a892
pjaselin <26674234+pjaselin@users.noreply.github.com>
2025-01-01 09:57:05 -0500

    simplified attribute usage script and removed unused return in _get_values

--- cubist/_attribute_usage.py
+++ cubist/_attribute_usage.py
@@ -4,42 +4,42 @@ import pandas as pd
 
 
 def _attribute_usage(output: str, feature_names: list | set):
-    # split output at newline character
-    output = output.split("\n")
     # get the attribute usage section of the model output
-    start_vars = [i for i, c in enumerate(output) if "\tAttribute usage" in c]
-    # if not present raise an error
-    if len(start_vars) != 1:
+    start_i = output.find("Attribute usage:")
+    if start_i == -1:
         raise ValueError("Cannot find attribute usage data")
+    end_i = min(
+        i
+        for i in [output.find("Time: "), output.find("Evaluation on test data")]
+        if i > 0
+    )
+
+    output = output[start_i + 17 : end_i].rstrip("\n")
+
     # keep only the Attribute usage section
-    output = [o for o in output[start_vars[0] : (len(output)) - 2] if o != ""]
+    output = [o for o in output.split("\n") if o != ""]
+    columns = output.pop(0).lstrip("\t").strip().split("  ")
+
     # per line in the Attribute usage, remove the tabs and strip
     # leading/trailing whitespace
-    output = [c.replace("\t", "").strip() for c in output]
-    has_pct = [i for i, c in enumerate(output) if "%" in c]
-    if len(has_pct) < 1:
-        return None
-    output = [output[i] for i in has_pct]
-    values = [_get_values(c) for c in output]
-    values = pd.DataFrame(values, columns=["Conditions", "Model"])
-    # split on 4 whitespaces and use last element in the list as the variable
-    values["Variable"] = [c.split("    ")[-1] for c in output]
-
+    output = [_parse_attribute(o.lstrip("\t").strip()) for o in output]
+    # left pad output
+    output = pd.DataFrame(output, columns=columns + ["Variable"])
     # check to see if there are more features than reported in the Attribute
     # usage table
-    if values.shape[0] < len(feature_names):
+    if output.shape[0] < len(feature_names):
         # get the list of missing features
-        missing_vars = list(set(feature_names) - set(values.Variable))
+        missing_vars = list(set(feature_names) - set(output.Variable))
         # create a list of zeros equal to the length of missing_vars
-        zero_list = [0] * len(missing_vars)
+        zero_list = [None] * len(missing_vars)
         # create a dataframe of zeros for the Conditions and Model columns for
         # each missing variable and append to values
-        values = pd.concat(
+        output = pd.concat(
             [
-                values,
+                output,
                 pd.DataFrame(
                     {
-                        "Conditions": zero_list,
+                        "Conds": zero_list,
                         "Model": zero_list,
                         "Variable": missing_vars,
                     }
@@ -47,23 +47,28 @@ def _attribute_usage(output: str, feature_names: list | set):
             ],
             axis=0,
         )
-    return values
+    return output
 
 
-def _get_values(x):
+def _parse_attribute(x):
+    # get the variable name as the string following the last occurrence of four
+    # whitespace characters
+    attribute_start_i = x.rindex("    ")
+    attribute_name = x[attribute_start_i:]
+    return _get_values(x[:attribute_start_i]) + [attribute_name]
+
+
+def _get_values(x) -> list[int, int]:
     x2 = x.split(" ")
     has_pct = ["%" in c for c in x2]
     if sum(has_pct) == 2:
         x2 = [x2[i] for i, c in enumerate(has_pct) if c]
-        x2 = [float(c.replace("%", "")) for c in x2]
-        return x2
-    if sum(has_pct) == 1:
-        pct_ind = [i for i, c in enumerate(x2) if "%" in c][0]
-        if x2[1 : pct_ind + 1].count("") < x2[pct_ind + 1 :].count(""):
-            x2 = [c for c in x2 if "%" in c][0]
-            x2 = float(x2.replace("%", ""))
-            return [x2, 0]
+        return [float(c.replace("%", "")) for c in x2]
+    pct_ind = [i for i, c in enumerate(x2) if "%" in c][0]
+    if x2[1 : pct_ind + 1].count("") < x2[pct_ind + 1 :].count(""):
         x2 = [c for c in x2 if "%" in c][0]
         x2 = float(x2.replace("%", ""))
-        return [0, x2]
-    return 0
+        return [x2, 0]
+    x2 = [c for c in x2 if "%" in c][0]
+    x2 = float(x2.replace("%", ""))
+    return [0, x2]
