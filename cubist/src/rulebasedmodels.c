c8c4033678cc35116d38dc08b2744a072cf34a49
pjaselin <26674234+pjaselin@users.noreply.github.com>
2026-01-31 16:15:37 -0500

    feat: add cv back

--- cubist/src/rulebasedmodels.c
+++ cubist/src/rulebasedmodels.c
@@ -170,7 +170,8 @@ void initglobals(void)
  * Set global variables in preparation for creating a model
  */
 void setglobals(int unbiased, char *composite, int neighbors, int committees,
-                double sample, int seed, int rules, double extrapolation) {
+                double sample, int seed, int rules, double extrapolation,
+                int cv) {
   /* XXX What about setting FOLDS? */
 
   UNBIASED = unbiased != 0 ? bintrue : binfalse;
